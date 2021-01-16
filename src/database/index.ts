0721235406038deaa8bb69739025af56bd55fda8
DragonCat || Kiara || Yumi <68660034+DragonCat4012@users.noreply.github.com>
2021-01-16 21:37:21 +0100

    Switching to localhost

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,5 +3,5 @@ import mongoose from 'mongoose';
 export * from './UserData';
 export * from './User';
 
-mongoose.connect('mongodb://192.168.1.24:27017/calq', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/calq', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log(' > Connected to DB'))
